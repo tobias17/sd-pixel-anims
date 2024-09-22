d628fe36172f3dcf8a95c83635455d9dcc8f6ca9
tobias17 <tobiasfischer17@gmail.com>
2024-09-22 13:04:04 -0400

    switched sheet_map to use re patterns

--- spritesheet.py
+++ spritesheet.py
@@ -1,11 +1,11 @@
-import os, argparse, math, cv2, json
+import os, argparse, cv2, json, re
 from typing import Optional, List
 import numpy as np
 
 def map_filename(filename:str, sheet_map:List[str]) -> Optional[str]:
-   for prefix in sheet_map:
-      if filename.startswith(prefix):
-         return prefix
+   for pattern in sheet_map:
+      if re.match(pattern, filename):
+         return pattern
    return None
 
 def spritesheet(in_path:str, save_path:str, sheet_map_path:Optional[int]):
@@ -18,40 +18,40 @@ def spritesheet(in_path:str, save_path:str, sheet_map_path:Optional[int]):
       with open(sheet_map_path, "r") as f:
          sheet_map = json.load(f)
       assert isinstance(sheet_map, list), f"Sheet map must be a list, got {type(sheet_map).__name__}"
-      for prefix in sheet_map:
-         assert isinstance(prefix, str), f"Sheet map must only contain str elements, found '{prefix}' of type {type(prefix).__name__}"
+      for pattern in sheet_map:
+         assert isinstance(pattern, str), f"Sheet map must only contain str elements, found '{pattern}' of type {type(pattern).__name__}"
    else:
       sheet_map = [""]
-   filename_to_prefix = { f: map_filename(f, sheet_map) for f in orig_filenames }
-   found_prefixes = set()
+   filename_to_pattern = { f: map_filename(f, sheet_map) for f in orig_filenames }
+   found_patternes = set()
    for filename in orig_filenames:
-      value = filename_to_prefix[filename]
+      value = filename_to_pattern[filename]
       if value is None:
          print(f"WARNING: Could not find mapping for filename {filename}, skipping")
-         filename_to_prefix.pop(filename)
+         filename_to_pattern.pop(filename)
       else:
-         found_prefixes.add(value)
-   assert len(found_prefixes) > 0, f"Found none of the prefixes, have no images to process"
-   if len(found_prefixes) < len(sheet_map):
-      print(f"WARNING: Missing entries from the following prefixes {list(set(sheet_map).difference(found_prefixes))}")
+         found_patternes.add(value)
+   assert len(found_patternes) > 0, f"Found none of the patternes, have no images to process"
+   if len(found_patternes) < len(sheet_map):
+      print(f"WARNING: Missing entries from the following patternes {list(set(sheet_map).difference(found_patternes))}")
 
-   filename_to_image = { f: cv2.imread(os.path.join(in_path, f), cv2.IMREAD_UNCHANGED) for f in filename_to_prefix.keys() }
+   filename_to_image = { f: cv2.imread(os.path.join(in_path, f), cv2.IMREAD_UNCHANGED) for f in filename_to_pattern.keys() }
    images = list(filename_to_image.values())
    assert len(images) > 0, "Found 0 *.png files at the in_path"
    assert all(len(images[0].shape) == len(im.shape) and images[0].shape == im.shape for im in images[1:]), "Images were not all the same size"
 
    IMG_H, IMG_W = images[0].shape[:2]
 
-   row_count = len(found_prefixes)
-   row_size  = max(sum(1 if found_prefix == check_prefix else 0 for found_prefix in filename_to_prefix.values()) for check_prefix in found_prefixes)
+   row_count = len(found_patternes)
+   row_size  = max(sum(1 if found_pattern == check_pattern else 0 for found_pattern in filename_to_pattern.values()) for check_pattern in found_patternes)
 
    sheet = np.zeros((IMG_H*row_count, IMG_W*row_size, images[0].shape[2]))
    print(sheet.shape)
    y = 0
-   for prefix in sheet_map:
-      if prefix not in found_prefixes:
+   for pattern in sheet_map:
+      if pattern not in found_patternes:
          continue
-      filenames = sorted(list(f for f,p in filename_to_prefix.items() if p == prefix))
+      filenames = sorted(list(f for f,p in filename_to_pattern.items() if p == pattern))
       x = 0
       for filename in filenames:
          sheet[y*IMG_H:(y+1)*IMG_H, x*IMG_W:(x+1)*IMG_W] = filename_to_image[filename]
@@ -64,7 +64,7 @@ if __name__ == "__main__":
    parser = argparse.ArgumentParser()
    parser.add_argument('--in-path',   type=str, required=True, help="Path to folder containing the images to package into a spritesheet")
    parser.add_argument('--save-path', type=str, required=True, help="Path to save the spritesheet at")
-   parser.add_argument('--sheet-map', type=str, help="Path to a json file defining which filename prefixes go in rows together")
+   parser.add_argument('--sheet-map', type=str, help="Path to a json file defining which filename patterns go in rows together")
    args = parser.parse_args()
 
    spritesheet(args.in_path, args.save_path, args.sheet_map)
